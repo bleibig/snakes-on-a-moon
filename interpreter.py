60b32251f88c1428519b1bb4e6985b56bf78cfa0
Brian Leibig <brian.leibig@gmail.com>
2011-06-25 13:57:02 -0400

    Added stubs for basic lua library functions.

--- interpreter.py
+++ interpreter.py
@@ -21,12 +21,12 @@ class Interpreter:
             inst = struct.unpack('i', inst_bytestring)[0]
             opcode = inst & ord('\x3f') # first 6 bits
             # # opcode type 1
-            # a = inst_unpacked & struct.unpack('i', '\x00\x00\x3f\xc0')[0]
-            # b = inst_unpacked & struct.unpack('i', '\x00\x7f\xc0\x00')[0]
-            # c = inst_unpacked & struct.unpack('i', '\xff\x80\x00\x00')[0]
+            # a = inst & struct.unpack('i', '\x00\x00\x3f\xc0')[0]
+            # b = inst & struct.unpack('i', '\x00\x7f\xc0\x00')[0]
+            # c = inst & struct.unpack('i', '\xff\x80\x00\x00')[0]
             # # opcode type 2/3
-            # a = inst_unpacked & struct.unpack('i', '\x00\x00\x3f\xc0')[0]
-            # bx = inst_unpacked & struct.unpack('i', '\xff\xff\xc0\x00')[0]
+            # a = inst & struct.unpack('i', '\x00\x00\x3f\xc0')[0]
+            # bx = inst & struct.unpack('i', '\xff\xff\xc0\x00')[0]
             if opcode == 0: # MOVE
                 # iABC instruction
                 print 'MOVE NYI'
@@ -70,52 +70,76 @@ class Interpreter:
                 # iABC instruction
                 print 'SUB NYI'
             elif opcode == 14: # MUL
+                # iABC instruction
                 print 'MUL NYI'
             elif opcode == 15: # DIV
+                # iABC instruction
                 print 'DIV NYI'
             elif opcode == 16: # MOD
+                # iABC instruction
                 print 'MOD NYI'
             elif opcode == 17: # POW
+                # iABC instruction
                 print 'POW NYI'
             elif opcode == 18: # UNM
+                # iABC instruction
                 print 'UNM NYI'
             elif opcode == 19: # NOT
+                # iABC instruction
                 print 'NOT NYI'
-            elif opcode == 20: #LEN
+            elif opcode == 20: # LEN
+                # iABC instruction
                 print 'LEN NYI'
             elif opcode == 21: # CONCAT
+                # iABC instruction
                 print 'CONCAT NYI'
             elif opcode == 22: # JMP
+                # iAsBx instruction
                 print 'JMP NYI'
             elif opcode == 23: # EQ
+                # iABC instruction
                 print 'EQ NYI'
             elif opcode == 24: # LT
+                # iABC instruction
                 print 'LT NYI'
             elif opcode == 25: # LE
+                # iABC instruction
                 print 'LE NYI'
             elif opcode == 26: # TEST
+                # iABC instruction
                 print 'TEST NYI'
             elif opcode == 27: # TESTSET
+                # iABC instruction
                 print 'TESTSET NYI'
             elif opcode == 28: # CALL
+                # iABC instruction
                 print 'CALL NYI'
             elif opcode == 29: # TAILCALL
+                # iABC instruction
                 print 'TAILCALL NYI'
             elif opcode == 30: # RETURN
+                # iABC instruction
                 print 'RETURN NYI'
             elif opcode == 31: # FORLOOP
+                # iAsBx instruction
                 print 'FORLOOP NYI'
             elif opcode == 32: # FORPREP
+                # iAsBx instruction
                 print 'FORPREP NYI'
             elif opcode == 33: # TFORLOOP
+                # iABC instruction
                 print 'TFORLOOP NYI'
             elif opcode == 34: # SETLIST
+                # iABC instruction
                 print 'SETLIST NYI'
             elif opcode == 35: # CLOSE
+                # iABC instruction
                 print 'CLOSE NYI'
             elif opcode == 36: # CLOSURE
+                # iABx instruction
                 print 'CLOSURE NYI'
             elif opcode == 37: # VARARG
+                # iABC instruction
                 print 'VARARG NYI'
 
 def main():
---
+++ library.py
@@ -0,0 +1,76 @@
+# basic library
+
+def lua_assert(v, message=None):
+    print 'assert NYI'
+
+def lua_collectgarbage(opt, arg=None):
+    print 'collectgarbage NYI'
+
+def lua_dofile(filename):
+    print 'dofile NYI'
+
+def lua_error(message, level=None):
+    print 'error NYI'
+
+def lua_getfenv(f=None):
+    print 'getfenv NYI'
+
+def lua_getmetatable(object_):
+    print 'getmetatable NYI'
+
+def lua_getipairs(t):
+    print 'getipairs NYI'
+
+def lua_load(func, chunkname=None):
+    print 'load NYI'
+
+def lua_loadfile(filename=None):
+    print 'loadfile NYI'
+
+def lua_loadstring(string, chunkname=None):
+    print 'loadstring NYI'
+
+def lua_next(table, index=None):
+    print 'next NYI'
+
+def lua_pairs(t):
+    print 'pairs NYI'
+
+def lua_pcall(f, args):
+    print 'pcall NYI'
+
+def lua_print(args):
+    print 'print NYI'
+
+def lua_rawequal(v1, v2):
+    print 'rawequal NYI'
+
+def lua_rawget(table, index):
+    print 'rawget NYI'
+
+def lua_rawset(table, index, value):
+    print 'rawset NYI'
+
+def lua_select(index, args):
+    print 'select NYI'
+
+def lua_setfenv(f, table):
+    print 'setfenv NYI'
+
+def lua_setmetatable(table, metatable):
+    print 'setmetatable NYI'
+
+def lua_tonumber(e, base=None):
+    print 'tonumber NYI'
+
+def lua_tostring(e):
+    print 'tostring NYI'
+
+def lua_type(v):
+    print 'type NYI'
+
+def lua_unpack(list_, i=None, j=None):
+    print 'unpack NYI'
+
+def lua_xpcall(f, err):
+    print 'xpcall NYI'
